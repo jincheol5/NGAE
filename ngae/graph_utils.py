5f434c6dabe0be9aa2d3a0f51ccfcb0c9f925e38
jincheol <oscar4293@gmail.com>
2025-08-10 17:26:16 +0900

    update

--- ngae/graph_utils.py
+++ ngae/graph_utils.py
@@ -321,15 +321,18 @@ class GraphUtils:
                 -value: Data 
             """
             data_dict={}
-            for graph_id,graph in tqdm(enumerate(graph_list),desc=f"Convert {graph_type} graph_list to data_dict"):
-                src_dict={}
-                if desc:
+            if desc:
+                for graph_id,graph in tqdm(enumerate(graph_list),desc=f"Convert {graph_type} graph_list to data_dict"):
+                    src_dict={}
                     for source_id in tqdm(graph.nodes(),desc=f"Convert {graph_type} {graph_id} graph to data..."):
                         src_dict[source_id]=GraphUtils.GraphProcessor.algo_trajectory_to_PyG_Data(graph=graph,graph_type=graph_type,graph_id=graph_id,source_id=source_id)
-                else:
+                    data_dict[graph_id]=src_dict
+            else:
+                for graph_id,graph in enumerate(graph_list):
+                    src_dict={}
                     for source_id in graph.nodes():
                         src_dict[source_id]=GraphUtils.GraphProcessor.algo_trajectory_to_PyG_Data(graph=graph,graph_type=graph_type,graph_id=graph_id,source_id=source_id)
-                data_dict[graph_id]=src_dict
+                    data_dict[graph_id]=src_dict
             return data_dict
 
         @staticmethod
